c7756d0eae079ecb6b946753be39a03c0201c10c
Matteo <matteo.sissa29@gmail.com>
2024-11-06 20:06:03 +0100

    Investments exercise

--- src/main/java/it/polimi/middleware/spark/batch/iterative/InvestmentSimulator.java
+++ src/main/java/it/polimi/middleware/spark/batch/iterative/InvestmentSimulator.java
@@ -4,6 +4,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import scala.Tuple2;
+
 /**
  * Start from a dataset of investments. Each element is a Tuple2(amount_owned, interest_rate).
  * At each iteration the new amount is (amount_owned * (1+interest_rate)).
@@ -21,12 +23,50 @@ public class InvestmentSimulator {
         final JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("ERROR");
 
-        final JavaRDD<String> textFile = sc.textFile(filePath + "files/iterative/investment.txt");
+        final JavaRDD<String> linesFromTextFile = sc.textFile(filePath + "files/iterative/investment.txt");
+
+
+        /***************************** SOLUTION **************************************/
+        /* 
+        The text file contains a list of items with this format
+            
+                3 0.01
+                4 0.015
+                2 0.05  
+        
+        First, let's turn the textual lines of the file, into a JavaRDD<Tuple2<Double, Double>> containing the numeric values.
+        */
+        JavaRDD<Tuple2<String, String>> textualInvestments = linesFromTextFile.map(
+
+            line -> new Tuple2(line.split(" ")[0], line.split(" ")[1])
+
+        );
+
+        JavaRDD<Tuple2<Double, Double>> investments = textualInvestments.map(
+
+            elem -> new Tuple2(Double.parseDouble(elem._1()), Double.parseDouble(elem._2()))
+
+        );
+
+
+        /*
+         * Now create a loop to update all the investments in the JavaRDD investments structure.
+         * At each iteration sum all the investments to verify whether to stop.
+         */
 
         int iteration = 0;
         double sum = 0;
 
-        // TODO
+        while (sum < threshold) {
+            // Update current values
+            investments.map(
+                elem -> new Tuple2<>(elem._1() * (1 + elem._2()), elem._2())
+            );
+
+            // Compute total sum of current investments
+            // Notice that map requires all inputs and outputs to be of the same type. mapToXXX makes you choose the target type
+            sum = investments.mapToDouble(inv -> inv._1()).sum();
+        }
 
         System.out.println("Sum: " + sum + " after " + iteration + " iterations");
         sc.close();
--- src/main/java/it/polimi/middleware/spark/batch/wordcount/WordCountModified.java
+++ src/main/java/it/polimi/middleware/spark/batch/wordcount/WordCountModified.java
@@ -1,8 +1,15 @@
 package it.polimi.middleware.spark.batch.wordcount;
 
+import java.util.Arrays;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+
 
 public class WordCountModified {
 
@@ -16,17 +23,39 @@ public class WordCountModified {
 
         final JavaRDD<String> lines = sc.textFile(filePath + "files/wordcount/in.txt");
 
-        // Q1. For each character, compute the number of words starting with that character
 
-        // TODO
+        // Q1. For each character, compute the number of words starting with that character
+        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> initials = words.map(w -> w.substring(0, 1).toLowerCase()).mapToPair(c->new Tuple2<>(c,1));
+        JavaPairRDD<String, Integer> counts = initials.reduceByKey((a, b) -> a + b);
+
+        System.out.println("WORDS-----------"+ counts.collect());
+
+
 
         // Q2. For each character, compute the number of lines starting with that character
 
-        // TODO
+        JavaPairRDD<String, Integer> lines_initials = lines.map(line -> line.substring(0, 1).toLowerCase()).mapToPair(c->new Tuple2<>(c,1));
+        JavaPairRDD<String, Integer> lines_counts = lines_initials.reduceByKey((a, b) -> a + b);
+
+        System.out.println("LINES-----------"+ lines_counts.collect());
 
         // Q3. Compute the average number of characters in each line
+        int total_characters = lines.map(line -> line.replace(" ", "").length()).reduce((a,b) -> a+b);
+        int total_lines = lines.map(line -> 1).reduce((a,b)-> a+b);
+        System.out.println("average-----------" + (double) total_characters / total_lines);
+        System.out.println("lines-----------" + total_lines);
 
-        // TODO
+        JavaRDD<Integer> count_characters_line = lines.map(line -> line.replace(" ", "").length());
+        System.out.println(count_characters_line.collect());
+        JavaPairRDD<Integer, Integer> character_lines = count_characters_line.mapToPair(e -> new Tuple2<>(e, 1));
+        System.out.println(character_lines.collect());
+        Tuple2<Integer, Integer> sums = character_lines.reduce((e1, e2) -> new Tuple2<>(e1._1 + e2._1, e1._2 + e2._2));
+
+        
+
+        double average = (double) sums._1 / sums._2;
+        System.out.println(average);
 
         sc.close();
     }
