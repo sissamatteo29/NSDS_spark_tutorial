65bbafc67845522340cff706febe3cfd20cb287a
Smrevilo <74360058+Smrevilo@users.noreply.github.com>
2024-11-17 22:01:52 +0100

    Update to eval_template

--- SPARK_EVALUATION_LAB/src/main/java/it/polimi/middleware/spark/Evaluation_lab.java
+++ SPARK_EVALUATION_LAB/src/main/java/it/polimi/middleware/spark/Evaluation_lab.java
@@ -1,5 +1,27 @@
 package it.polimi.middleware.spark;
 
-public class Evaluation_lab {
+import org.apache.spark.sql.SparkSession;
 
+public class Evaluation_lab {
+    private static final boolean useCache = true;
+
+
+    private static void main(String[] args) {
+        final String master = args.length > 0 ? args[0] : "local[4]";
+        final String filePath = args.length > 1 ? args[1] : "./";
+        final String appName = useCache ? "Cache" : "NoCache";
+
+        final SparkSession spark = SparkSession
+                .builder()
+                .master(master)
+                .appName("SparkEval")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+
+
+
+
+
+
+    }
 }
